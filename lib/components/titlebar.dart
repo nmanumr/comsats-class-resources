89a0043c63639f28b4de8008fbefcdcf4462a947
namnumr <nmanumr@gmail.com>
2019-06-20 01:36:35 +0500

    replace more with search button

--- lib/components/titlebar.dart
+++ lib/components/titlebar.dart
@@ -6,20 +6,9 @@ AppBar titleBar(BuildContext context) {
     elevation: 1.5,
     actions: <Widget>[
       // overflow menu
-      PopupMenuButton<Choice>(
-        onSelected: (Choice choice) {},
-        itemBuilder: (BuildContext context) {
-          return choices.skip(2).map((Choice choice) {
-            return PopupMenuItem<Choice>(
-                value: choice,
-                child: Row(
-                  children: <Widget>[
-                    Icon(choice.icon),
-                    Text(choice.title),
-                  ],
-                ));
-          }).toList();
-        },
+      IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () {},
       ),
     ],
     bottom: TabBar(
@@ -34,19 +23,3 @@ AppBar titleBar(BuildContext context) {
     ),
   );
 }
-
-class Choice {
-  const Choice({this.title, this.icon});
-
-  final String title;
-  final IconData icon;
-}
-
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Toggle Dark', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
-  const Choice(title: 'Boat', icon: Icons.directions_boat),
-  const Choice(title: 'Bus', icon: Icons.directions_bus),
-  const Choice(title: 'Train', icon: Icons.directions_railway),
-  const Choice(title: 'Walk', icon: Icons.directions_walk),
-];
