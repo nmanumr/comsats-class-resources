37593e978f13935ecada7cf40627c0884c7f13fa
nmanumr <nmanumr@gmail.com>
2019-07-19 10:58:36 +0500

    app main

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,42 @@
-import 'package:class_resources/pages/login.dart';
-import 'package:class_resources/pages/reset-pass.dart';
-import 'package:class_resources/pages/signup.dart';
-import 'package:class_resources/services/authentication.dart';
 import 'package:flutter/material.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 import './themes.dart';
-import './pages/main.dart';
+import './pages/dashboard.dart';
+import './pages/welcome.dart';
+import './pages/signup.dart';
+import './pages/login.dart';
+import './pages/update-profile.dart';
 
-void main() {
-  return runApp(
-    MyApp(),
-  );
+void main() async {
+  try {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String uid = prefs.getString('uid') ?? "";
+    return runApp(AppMain(uid: uid));
+  } catch (_) {
+    return runApp(AppMain(uid: ""));
+  }
 }
 
-enum AuthStatus {
-  RESET_PASS,
-  SIGNUP,
-  NOT_DETERMINED,
-  NOT_LOGGED_IN,
-  LOGGED_IN,
-}
-
-class MyApp extends StatefulWidget {
-  final auth = new AuthService();
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  AuthStatus authStatus = AuthStatus.NOT_DETERMINED;
-  String _userId = "";
-
-  @override
-  void initState() {
-    super.initState();
-    widget.auth.getCurrentUser().then((user) {
-      setState(() {
-        if (user != null) {
-          _userId = user?.uid;
-        }
-        authStatus =
-            user?.uid == null ? AuthStatus.NOT_LOGGED_IN : AuthStatus.LOGGED_IN;
-      });
-    });
-  }
-
-  void _onLoggedIn() {
-    widget.auth.getCurrentUser().then((user) {
-      setState(() {
-        _userId = user.uid.toString();
-      });
-    });
-    setState(() {
-      authStatus = AuthStatus.LOGGED_IN;
-    });
-  }
-
-  void _onSignedOut() {
-    setState(() {
-      widget.auth.signOut();
-      authStatus = AuthStatus.NOT_LOGGED_IN;
-      _userId = "";
-    });
-  }
-
-  void _onResetPass() {
-    setState(() {
-      authStatus = AuthStatus.RESET_PASS;
-    });
-  }
-
-  void _toSignup() {
-    setState(() {
-      authStatus = AuthStatus.SIGNUP;
-    });
-  }
-
-  Widget _buildWaitingScreen() {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(),
-      ),
-    );
-  }
+class AppMain extends StatelessWidget {
+  AppMain({@required this.uid});
+  final String uid;
 
   @override
   Widget build(BuildContext context) {
-    Widget screen;
-
-    switch (authStatus) {
-      case AuthStatus.NOT_DETERMINED:
-        screen = _buildWaitingScreen();
-        break;
-      case AuthStatus.NOT_LOGGED_IN:
-        screen = new LoginPage(
-            auth: widget.auth,
-            onSignedIn: _onLoggedIn,
-            onResetPass: _onResetPass,
-            toSignup: _toSignup);
-        break;
-      case AuthStatus.LOGGED_IN:
-        if (_userId.length > 0 && _userId != null) {
-          screen = MainScreen(onSignOut: _onSignedOut);
-        } else {
-          screen = _buildWaitingScreen();
-        }
-        break;
-      case AuthStatus.RESET_PASS:
-        screen = ResetPassPage(
-          auth: widget.auth,
-          toLogin: _onSignedOut,
-        );
-        break;
-      case AuthStatus.SIGNUP:
-        screen = SignupPage(
-          auth: widget.auth,
-          onSignedIn: _onLoggedIn,
-          toLogin: _onSignedOut,
-        );
-        break;
-      default:
-        screen = _buildWaitingScreen();
-    }
-
     return MaterialApp(
       title: 'Class Resources',
       theme: lightTheme(),
       darkTheme: darkTheme(),
-      home: screen,
-      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (ctx) => this.uid == "" ? WelcomePage() : Dashboard(),
+        '/welcome': (ctx) => WelcomePage(),
+        '/dashboard': (ctx) => Dashboard(),
+        '/signup': (ctx) => SignupPage(),
+        '/login': (ctx) => LoginPage(),
+        '/edit-profile': (ctx) => UpdateProfile(),
+      },
     );
   }
 }
