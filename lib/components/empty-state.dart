eca4eb02ae5c744cefc5498fb638adb9985c31db
nmanumr <nmanumr@gmail.com>
2019-07-22 12:16:31 +0500

    EmptyState widget

---
+++ lib/components/empty-state.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+
+  EmptyState({this.icon, this.text, this.button});
+
+  final IconData icon;
+  final String text;
+  final Widget button;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Column(
+            children: <Widget>[
+              Icon(
+                this.icon,
+                size: 65,
+                color: Theme.of(context).iconTheme.color.withAlpha(100),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  this.text,
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.body1.color.withAlpha(180),
+                  ),
+                ),
+              ),
+              this.button
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
