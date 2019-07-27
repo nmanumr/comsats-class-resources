d596efa86b26757a9466273a0b86fa35207ebb93
nmanumr <nmanumr@gmail.com>
2019-07-27 13:03:01 +0500

    added timetable calender

--- lib/pages/timetable.dart
+++ lib/pages/timetable.dart
@@ -1,16 +1,238 @@
 import 'package:class_resources/components/centered-appbar.dart';
+import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+final Map<DateTime, List> _holidays = {
+  DateTime(2019, 1, 1): ['New Year\'s Day'],
+  DateTime(2019, 1, 6): ['Epiphany'],
+  DateTime(2019, 2, 14): ['Valentine\'s Day'],
+  DateTime(2019, 4, 21): ['Easter Sunday'],
+  DateTime(2019, 4, 22): ['Easter Monday'],
+};
 
 class TimeTablePage extends StatefulWidget {
   @override
   _TimeTablePageState createState() => _TimeTablePageState();
 }
 
-class _TimeTablePageState extends State<TimeTablePage> {
+class _TimeTablePageState extends State<TimeTablePage>
+    with TickerProviderStateMixin {
+  DateTime _selectedDay = DateTime.now();
+  Map<DateTime, List> _events;
+  Map<DateTime, List> _visibleEvents;
+  Map<DateTime, List> _visibleHolidays;
+  List _selectedEvents;
+  AnimationController _controller;
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = DateTime.now();
+    _events = {
+      _selectedDay.subtract(Duration(days: 30)): [
+        'Event A0',
+        'Event B0',
+        'Event C0'
+      ],
+      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+      _selectedDay.subtract(Duration(days: 20)): [
+        'Event A2',
+        'Event B2',
+        'Event C2',
+        'Event D2'
+      ],
+      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+      _selectedDay.subtract(Duration(days: 10)): [
+        'Event A4',
+        'Event B4',
+        'Event C4'
+      ],
+      _selectedDay.subtract(Duration(days: 4)): [
+        'Event A5',
+        'Event B5',
+        'Event C5'
+      ],
+      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      _selectedDay.add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      _selectedDay.add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      _selectedDay.add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      _selectedDay.add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      _selectedDay.add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+
+    _selectedEvents = _events[_selectedDay] ?? [];
+    _visibleEvents = _events;
+    _visibleHolidays = _holidays;
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _controller.forward();
+  }
+
+  void _onDaySelected(DateTime day, List events) {
+    setState(() {
+      _selectedDay = day;
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    setState(() {
+      _visibleEvents = Map.fromEntries(
+        _events.entries.where(
+          (entry) =>
+              entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
+              entry.key.isBefore(last.add(const Duration(days: 1))),
+        ),
+      );
+
+      _visibleHolidays = Map.fromEntries(
+        _holidays.entries.where(
+          (entry) =>
+              entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
+              entry.key.isBefore(last.add(const Duration(days: 1))),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: centeredAppBar(context, "Time Table")
+      appBar: centeredAppBar(context, "Time Table", actions: [
+        IconButton(
+          icon: Icon(Icons.today),
+          onPressed: () {
+            setState(() {
+              _selectedDay = DateTime.now();
+              _selectedEvents = _events[_selectedDay] ?? [];
+            });
+          },
+        )
+      ]),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          _buildTableCalendar(),
+          const SizedBox(height: 8.0),
+          Expanded(child: _buildEventList()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color(0xFF5EBBC8),
+      ),
+      width: 7.0,
+      height: 7.0,
+    );
+  }
+
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      selectedDay: _selectedDay,
+      events: _visibleEvents,
+      holidays: _visibleHolidays,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      initialCalendarFormat: CalendarFormat.twoWeeks,
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+        CalendarFormat.twoWeeks: '2 weeks',
+      },
+      calendarStyle: CalendarStyle(
+          selectedColor: Theme.of(context).accentColor.withAlpha(155),
+          todayColor: Theme.of(context).accentColor.withAlpha(40),
+          markersColor: Color(0xFF5EBBC8),
+          weekendStyle: TextStyle(color: Colors.white.withAlpha(200)),
+          outsideWeekendStyle: TextStyle(color: Colors.white.withAlpha(100)),
+          weekdayStyle: TextStyle(color: Colors.white),
+          holidayStyle: TextStyle(color: Colors.white.withAlpha(100))),
+      headerStyle: HeaderStyle(
+        formatButtonTextStyle:
+            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Theme.of(context).accentColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        leftChevronIcon: Icon(Icons.chevron_left),
+        rightChevronIcon: Icon(Icons.chevron_right),
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekdayStyle: TextStyle(color: Colors.white.withAlpha(180)),
+        weekendStyle: TextStyle(color: Colors.white.withAlpha(180)),
+      ),
+      builders: CalendarBuilders(
+        markersBuilder: (context, date, events, holidays) {
+          var children = <Widget>[];
+          if (events.isNotEmpty) {
+            children.add(
+              Positioned(
+                bottom: 10,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: _buildEventsMarker(date, events),
+                ),
+              ),
+            );
+          }
+          return children;
+        },
+      ),
+      onDaySelected: _onDaySelected,
+      onVisibleDaysChanged: _onVisibleDaysChanged,
+    );
+  }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: ListTile(
+                  title: Text(event.toString()),
+                  onTap: () => print('$event tapped!'),
+                ),
+              ))
+          .toList(),
     );
   }
 }
