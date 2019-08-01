b35fe5d20ac9d5a5994d6870483ac51b485bcffe
nmanumr <nmanumr@gmail.com>
2019-08-01 08:55:05 +0500

    bug fixed

--- lib/pages/courses/resources.dart
+++ lib/pages/courses/resources.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CourseResources extends StatelessWidget {
-
   CourseResources({this.model});
 
   final CourseModel model;
@@ -14,33 +13,30 @@ class CourseResources extends StatelessWidget {
     return Text('Error: $err');
   }
 
-  Widget onSuccess(DocumentSnapshot doc) {
-    return Builder(
-      builder: (BuildContext ctx) {
-        return ListTile(
-          leading: TextAvatar(
-            text: doc.data['title'] + doc.data['class'].documentID,
-          ),
-          title: Text(doc.data['title'] ?? ""),
-          subtitle: Text("${doc.data['class'].documentID} - ${doc.data['teacher']}" ?? ""),
-          // trailing: _getPopup(doc.reference),
-          onTap: () {},
-          // onLongPress: () {
-          //   if ((widget.popupItems ?? []).isNotEmpty) {
-          //     dynamic popUpMenustate = _menuKey.currentState;
-          //     popUpMenustate.showButtonMenu();
-          //   }
-          // },
-        );
-      },
+  Widget onSuccess(QuerySnapshot query) {
+    List<Widget> children = query.documents
+        .map((doc) => ListTile(
+              leading: TextAvatar(
+                text: doc.data['title'] + doc.data['class'].documentID,
+              ),
+              title: Text(doc.data['title'] ?? ""),
+              subtitle: Text(
+                  "${doc.data['class'].documentID} - ${doc.data['teacher']}" ??
+                      ""),
+              onTap: () {},
+            ))
+        .toList();
+
+    return ListView(
+      children: children,
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: model.getCourseResources(),
-      builder: (context, snapshot){
+      builder: (context, snapshot) {
         if (snapshot.hasError) return onError(snapshot.error);
 
         if (snapshot.connectionState == ConnectionState.waiting)
