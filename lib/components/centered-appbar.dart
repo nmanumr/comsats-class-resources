4799b35296c814564b70f8190e8e0cad8950b83e
nmanumr <nmanumr@gmail.com>
2019-08-06 16:11:54 +0500

    appbar leading

--- lib/components/centered-appbar.dart
+++ lib/components/centered-appbar.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 
-AppBar centeredAppBar(BuildContext ctx, String title, {List<Widget> actions}){
+AppBar centeredAppBar(BuildContext ctx, String title, {List<Widget> actions, Widget leading}){
   return AppBar(
       title: Text(title),
       centerTitle: true,
       backgroundColor: Theme.of(ctx).canvasColor,
       elevation: 0,
       actions: actions,
+      leading: leading
     );
 }
