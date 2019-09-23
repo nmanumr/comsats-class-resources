935c6034a3c958b5a710cabe3937e9ad572a779f
nmanumr <nmanumr@gmail.com>
2019-09-23 23:28:03 +0500

    prepared for v1.4

--- lib/flutter_version.dart
+++ lib/flutter_version.dart
@@ -1,10 +1,11 @@
 const Map<String, String> version =
 {
-  "frameworkVersion": "1.10.3-pre.51",
+  "frameworkVersion": "1.10.6-pre.19",
   "channel": "master",
   "repositoryUrl": "https://github.com/flutter/flutter.git",
-  "frameworkRevision": "0f00f424fa1f2044bcf5eadbf87dba01bda8385d",
-  "frameworkCommitDate": "2019-09-16 08:02:04 -0700",
-  "engineRevision": "3da554934b84df310d9d736cc1e8ea473982314b",
-  "dartSdkVersion": "2.6.0 (build 2.6.0-dev.0.0 f8f0b845c2)"
-};
+  "frameworkRevision": "ec07e039c202c7fa407b931348bb783175730ed2",
+  "frameworkCommitDate": "2019-09-19 21:37:41 -0400",
+  "engineRevision": "70f21b0edb0636fac38127b49064d03beb0a1ed8",
+  "dartSdkVersion": "2.6.0 (build 2.6.0-dev.1.0 7c1821c4aa)"
+}
+;
