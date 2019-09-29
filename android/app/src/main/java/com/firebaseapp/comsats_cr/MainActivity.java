4ac3904923ad5a51224bbef88bc4d7a4bb3cbaae
Ameer Hamza Naveed <humzaa2z@gmail.com>
2019-09-29 11:46:23 +0500

    updated timetable methodchannel

--- android/app/src/main/java/com/firebaseapp/comsats_cr/MainActivity.java
+++ android/app/src/main/java/com/firebaseapp/comsats_cr/MainActivity.java
@@ -8,6 +8,7 @@ import com.firebaseapp.comsats_cr.objects.Event;
 import com.firebaseapp.comsats_cr.widgets.TimeTableWidget;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
@@ -16,25 +17,26 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
 
-  private static final String CHANNEL = "com.firebaseapp.comsats_cr/timetable_widget";
+    private static final String CHANNEL = "com.firebaseapp.comsats_cr/timetable_widget";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
 
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler((call, result) -> {
-      if (call.method.equals("refresh_timetable_widget")) {
-        try {
-          TimeTableWidget.sendRefreshBroadcast(getApplicationContext());
-        } catch (Exception e) {
-          result.error("-1", "unexpected Error", null);
-        }
-      }else if(call.method.equals("update_timetable_widget")){
-        //TODO:: ADD CODE
-      }else{
-        result.notImplemented();
-      }
-    });
-  }
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler((call, result) -> {
+            if (call.method.equals("refresh_timetable_widget"))
+                TimeTableWidget.sendRefreshBroadcast(getApplicationContext());
+            else if(call.method.equals("update_timetable_widget")){
+                if(call.argument("todaysTimetableEvents") != null) {
+                    TimeTableWidget.timetable.clear();
+                    TimeTableWidget.timetable.addAll(Objects.requireNonNull(call.argument("todaysTimetableEvents")));
+                    TimeTableWidget.sendRefreshBroadcast(getApplicationContext());
+                    result.success("1");
+                }else
+                    result.notImplemented();
+            }else
+                result.notImplemented();
+        });
+    }
 }
