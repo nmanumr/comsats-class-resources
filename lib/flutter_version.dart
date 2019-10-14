3719cf00258f044c3ee5067350a0a042632cef5c
nmanumr <nmanumr@gmail.com>
2019-10-14 20:59:14 +0500

    update flutter version

--- lib/flutter_version.dart
+++ lib/flutter_version.dart
@@ -1,11 +1,11 @@
 const Map<String, String> version =
 {
-  "frameworkVersion": "1.10.15-pre.79",
+  "frameworkVersion": "1.10.15-pre.95",
   "channel": "master",
   "repositoryUrl": "https://github.com/flutter/flutter.git",
-  "frameworkRevision": "72d5c1c805987550717f9837555082bfe12f8c5e",
-  "frameworkCommitDate": "2019-10-11 03:58:43 -0400",
-  "engineRevision": "8dabfb90a11052d297cb217ff9889365dadcc6d7",
+  "frameworkRevision": "9423a01204ba9abf2e4b26fda3d5ef06d6897ef9",
+  "frameworkCommitDate": "2019-10-13 18:41:22 -0700",
+  "engineRevision": "eed171ff3538aa44f061f3768eec3a5908e8e852",
   "dartSdkVersion": "2.6.0 (build 2.6.0-dev.7.0 965b8cb1d8)"
 }
 ;
